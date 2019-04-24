33afa6c9591dfc4adb653d077021b78271b5a131
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-04-24 17:07:04 +0200

    initialized databaseHandler. (empty methods)

--- Logic/DatabaseHandler.cs
+++ Logic/DatabaseHandler.cs
@@ -4,8 +4,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using SEP_DataAPI.Model;
 
 namespace SEP_DataAPI.Logic
 {
+    public class DatabaseHandler
+    {
+        public void getAllAccs()
+        {
 
+        }
+
+        public void getAccById(int id)
+        {
+            
+        }
+
+        public void editAccount(Account Account)
+        {
+            
+        }
+
+        public void removeAccount(int id)
+        {
+            
+        }
+
+        public void getEnvironmentalData()
+        {
+            
+        }
+
+    }
 }
