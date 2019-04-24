bfbb21a3f593d058b8ccfad7865c843e2a266ae5
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-04-24 17:14:04 +0200

    added Newtonsoft

--- Controllers/GetAllAccountsController.cs
+++ Controllers/GetAllAccountsController.cs
@@ -13,14 +13,7 @@ namespace SEP_DataAPI.Controllers
     [ApiController]
     public class GetAllAccountsController : ControllerBase
     {
-    /*
-    *    If a request arrives for this endpoint, this method will run. Every writeline is to better understand the program while running
-    *    intentionally having a single post request, as other functions are located in other endpoints 
-    *    1, create an object that the incoming object will be stored in
-    *    2, deserialize the incoming Json to the specified object
-    *    3, if the command (intention of presentation layer) matches a possible function, call that function.
-    */
-        // POST api/values
+        // GET /GetAllAccounts
         [HttpGet]
         public ActionResult<string> Get()
         {
---
+++ Logic/JsonHandler.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SEP_DataAPI.Model;
+
+namespace SEP_DataAPI.Logic
+{
+    public class JsonHandler
+    {
+        
+    }
+}
